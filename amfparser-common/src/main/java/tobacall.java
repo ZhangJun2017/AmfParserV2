a19475908cf3b25410e07ed3f793b33f955a7b87
ZhangJun-ZhangJun_PC <sn2015.zhang@gmail.com>
2019-08-15 14:04:03 +0800

    Initial Commit

---
+++ amfparser-common/src/main/java/tobacall.java
@@ -0,0 +1,14 @@
+/**
+ * amfparser-common/tobecall.java
+ * Description:
+ *
+ * @author:ZhangJun2017
+ * @date:2019/8/14
+ */
+
+public class tobacall {
+    public static void callme(){
+        System.out.println("I'm from 'tobecall' !");
+        System.out.println("v3");
+    }
+}
---
+++ amfparser-parser/src/main/java/test.java
@@ -0,0 +1,13 @@
+/**
+ * amfparser-parser/test.java
+ * Description:
+ *
+ * @author:ZhangJun2017
+ * @date:2019/8/14
+ */
+
+public class test {
+    public static void main(String[] args){
+        tobacall.callme();
+    }
+}
