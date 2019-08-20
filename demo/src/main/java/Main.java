e9c34b2a0331e2f2246d0b3390c619b861c3618b
ZhangJun-ZhangJun_PC <sn2015.zhang@gmail.com>
2019-08-20 15:56:43 +0800

    190820:Added module 'demo'

---
+++ demo/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import io.zhangjun2017.amfparser.common.Status;
+
+/**
+ * demo/Main.java
+ * Description:
+ *
+ * @author:ZhangJun2017
+ * @date:2019/8/20
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Status(123, "456"));
+    }
+}
